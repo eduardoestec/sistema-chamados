e197ce0f66b8fc7e122e9fc6b06267a6673bd78e
EduardoEStec <e.s.tecnologiai.eduardo@gmail.com>
2026-04-02 00:54:10 -0300

    fix: rename supabase file

--- app/api/chamados/route.ts
+++ app/api/chamados/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { supabase } from '@/lib/supabase'
+import { supabase } from '@/lib/supabaseClient'
 
 function gerarCodigo() {
   const num = Math.floor(Math.random() * 9000) + 1000
